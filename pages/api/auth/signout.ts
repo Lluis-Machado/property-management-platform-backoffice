e54162b6e666f9cf2d0d5d60d500ac0942f44ce6
Lisandro Rocha <lisandro@itcplatform.de>
2023-06-19 09:09:44 +0200

    fix logout prevent vercel caching req

--- pages/api/auth/signout.ts
+++ pages/api/auth/signout.ts
@@ -4,6 +4,7 @@ import { withSessionRoute } from "@/lib/ironSession/withSession"
 export default withSessionRoute(logoutRoute)
 
 async function logoutRoute(req: NextApiRequest, res: NextApiResponse) { 
-    req.session.destroy()
-    return res.status(200).send("OK")
+    req.session.destroy();
+    res.setHeader("cache-control", "no-store, max-age=0");
+    return res.status(200).send("OK");
 }
