5707dc9aa34b1309bddb622cc1af8787c6ac2e16
Lisandro Rocha <lisandro@itcplatform.de>
2023-06-22 11:56:28 +0200

    micro fixes

--- lib/utils/datagrid/localeDevExtreme.ts
+++ lib/utils/datagrid/localeDevExtreme.ts
@@ -1,24 +1,21 @@
 // Libraries imports
 import { loadMessages, locale } from 'devextreme/localization';
-import { match } from '@formatjs/intl-localematcher';
 import deMessages from 'devextreme/localization/messages/de.json';
 import enMessages from 'devextreme/localization/messages/en.json';
 import esMessages from 'devextreme/localization/messages/es.json';
 
 // Local imports
-import { i18n } from '@/i18n-config';
-
-export const localeDevExtreme = () => {
-    // @ts-ignore
-    const navigatorLanguage = match(navigator.languages, i18n.locales, i18n.defaultLocale);
-    switch (navigatorLanguage) {
+import { Locale } from '@/i18n-config';
+  
+export const localeDevExtreme = (lang: Locale) => {
+    switch (lang) {
         case 'de':
             loadMessages(deMessages)
-            locale(navigatorLanguage)
+            locale(lang)
             break;
         case 'es':
             loadMessages(esMessages)
-            locale(navigatorLanguage)
+            locale(lang)
             break;
         default:
             loadMessages(enMessages)
