7869572c7e3f3ca98f49ecbebc2fe83ac496e5b7
Marieke <marieke.linnemann@plattesgroup.net>
2023-06-22 16:11:19 +0200

    Added Accounting Page

--- app/[lang]/private/accounting/page.tsx
+++ app/[lang]/private/accounting/page.tsx
@@ -1,5 +1,19 @@
-import { redirect } from 'next/navigation';
+'use client'
+
+//React imports
+//Library imports
+
+// Local imports
+import AccountingWrapper from "@/components/datagrid/accountingDatagrid/AccountingWrapper"
+import data from '@/components/datagrid/propertiesDatagrid/data.json';
 
 export default async function Accounting() {
-    redirect('./accounting/tenants');
+    return (
+        <>
+            <div className='text-l text-secondary-500 ml-8 mt-4'>Accounting</div>
+            <AccountingWrapper
+                dataSource={data}
+            />
+        </>
+    )
 }
---
+++ components/datagrid/datagrid/Datagrid.tsx
@@ -0,0 +1,68 @@
+// react imports
+
+//library imports
+import DataGrid, { Column as DxColumn, SearchPanel, Toolbar, Item, Pager, HeaderFilter } from 'devextreme-react/data-grid';
+
+// local imports
+
+
+interface Props {
+    dataSource: any[];
+    handleDouleClick: ({ data }: any) => void;
+};
+
+const Datagrid = ({ dataSource, handleDouleClick }: Props): React.ReactElement => {
+    return (
+        <DataGrid
+            allowColumnResizing={true}
+            columnHidingEnabled={false}
+            columnMinWidth={100}
+            dataSource={dataSource}
+            focusedRowEnabled
+            id='datagrid'
+            keyExpr='name'
+            showBorders
+            showRowLines
+            onRowDblClick={handleDouleClick}
+        >
+            <HeaderFilter visible />
+            <SearchPanel
+                searchVisibleColumnsOnly={false}
+                visible
+                width={350}
+            />
+            <Pager
+                allowedPageSizes='auto'
+                showInfo
+                showNavigationButtons
+                visible
+            />
+
+            <Toolbar>
+                <Item name='searchPanel' />
+            </Toolbar>
+
+            <DxColumn
+                caption='Name'
+                dataField='name'
+                dataType='string'
+                hidingPriority={0}
+            />
+            <DxColumn
+                caption='Contact Person'
+                dataField='primary_contact'
+                dataType='string'
+                hidingPriority={1}
+            />
+            <DxColumn
+                caption='Address'
+                dataField='address'
+                dataType='string'
+                allowHeaderFiltering={false}
+                hidingPriority={2}
+            />
+        </DataGrid>
+    )
+}
+
+export default Datagrid
