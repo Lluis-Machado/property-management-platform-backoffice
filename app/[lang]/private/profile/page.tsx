b809aca680c9ebb6bc25fb43487e4196e6dbfe30
Lisandro Rocha <lisandro@itcplatform.de>
2023-06-27 11:57:21 +0200

    Basic Profile page created

--- app/[lang]/private/profile/page.tsx
+++ app/[lang]/private/profile/page.tsx
@@ -1,8 +1,10 @@
-import React from 'react'
+import ProfilePage from "@/components/pages/ProfilePage"
 
 const page = () => {
   return (
-    <div>PROFILE</div>
+    <>
+      <ProfilePage />
+    </>
   )
 }
 
