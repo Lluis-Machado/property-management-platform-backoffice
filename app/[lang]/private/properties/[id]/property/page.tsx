869d6ef528af68b4ee60df0900b50da8b0aa99c6
sergioVegaPG <sergio.vega@plattesgroup.net>
2023-06-30 15:04:49 +0200

    properties

--- app/[lang]/private/properties/[id]/property/page.tsx
+++ app/[lang]/private/properties/[id]/property/page.tsx
@@ -4,13 +4,13 @@ interface Props {
   params: { id: string };
 };
 
-const page = ({ params: { id } }: Props): React.ReactElement => {
-  return (
-    <>
-      <div className='text-l text-secondary-500 mb-3'>{`Properties / ${id}`}</div>
-      <PropertyFormInfo/>
-    </>
-  );
-};
+const page = ({ params: { id } }: Props): React.ReactElement => (
+  <>
+    <div className='text-l text-secondary-500 mb-3'>
+      {`Properties / ${id}`}
+    </div>
+    <PropertyFormInfo />
+  </>
+);
 
 export default page;
