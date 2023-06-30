bc2377c9e852eda38a4fe928083d66d71d718098
sergioVegaPG <sergio.vega@plattesgroup.net>
2023-06-30 12:55:26 +0200

    popup fix

--- app/[lang]/private/accounting/[id]/expenses/page.tsx
+++ app/[lang]/private/accounting/[id]/expenses/page.tsx
@@ -1,44 +1,47 @@
 'use client'
+
 // React imports
 import { useState } from 'react';
 
 //Local imports
-import data from '@/components/datagrid/apinvoicesDatagrid/data.json';
-import ApInvoicesDatagrid from '@/components/datagrid/apinvoicesDatagrid/ApInvoicesDatagrid';
-import PopupPreview from '@/components/popups/PopupPreview';
 import { Locale } from '@/i18n-config';
+import { PopupVisibility } from '@/lib/types/Popups';
+import ApInvoicesDatagrid from '@/components/datagrid/apinvoicesDatagrid/ApInvoicesDatagrid';
+import data from '@/components/datagrid/apinvoicesDatagrid/data.json';
+import PopupPreview from '@/components/popups/PopupPreview';
+
 interface Props {
   params: { lang: Locale }
-  searchParams: {searchParams: any}
-}
+  searchParams: { searchParams: any }
+};
 
-export default function ApInvoices({ params: {lang}, searchParams }: Props): React.ReactElement {
-  const [isInvoicePreviewVisible, setIsInvoicePreviewVisible] = useState<boolean>(false);
+export default function ApInvoices({ params: { lang }, searchParams }: Props): React.ReactElement {
+  const [invoiceVisibility, setInvoiceVisibility] = useState<PopupVisibility>({ hasBeenOpen: false, visible: false });
   const [invoicePreviewTitle, setInvoicePreviewTitle] = useState<string>('');
   const [invoicePreviewURL, setInvoicePreviewURL] = useState<string>('');
   return (
     <div>
       <div className='text-l text-secondary-500 mt-4'>Accounting / AP Invoices</div>
-      <ApInvoicesDatagrid  
-           dataSource={data}
-           onInvoiceClick={(title, url) => {
-               setInvoicePreviewTitle(title);
-               setInvoicePreviewURL(url);
-               setIsInvoicePreviewVisible(true);
-           }}
-           params={searchParams}
-           lang={lang}
+      <ApInvoicesDatagrid
+        dataSource={data}
+        onInvoiceClick={(title, url) => {
+          setInvoicePreviewTitle(title);
+          setInvoicePreviewURL(url);
+          setInvoiceVisibility(p => ({ ...p, visible: true }));
+        }}
+        params={searchParams}
+        lang={lang}
       />
-                  {
-                isInvoicePreviewVisible &&
-                <PopupPreview
-                    fileURL={invoicePreviewURL}
-                    isVisible={isInvoicePreviewVisible}
-                    onClose={() => setIsInvoicePreviewVisible(false)}
-                    title={invoicePreviewTitle}
-                />
-            }
+      {
+        (invoiceVisibility.visible || invoiceVisibility.hasBeenOpen) &&
+        <PopupPreview
+          fileURL={invoicePreviewURL}
+          isVisible={invoiceVisibility.visible}
+          onClose={() => setInvoiceVisibility(p => ({ ...p, visible: false }))}
+          title={invoicePreviewTitle}
+          onShown={() => setInvoiceVisibility(p => ({ ...p, hasBeenOpen: true }))}
+        />
+      }
     </div>
-  )
-}
-
+  );
+};
