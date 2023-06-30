e2cbec6fae482a207c894bd50c5bb2226bf08c5a
sergioVegaPG <sergio.vega@plattesgroup.net>
2023-06-30 08:58:32 +0200

    smol fix

--- app/[lang]/private/layout.tsx
+++ app/[lang]/private/layout.tsx
@@ -2,7 +2,6 @@
 import '@/lib/assets/dx.generic.pg-theme.css';
 import { Locale } from '@/i18n-config';
 import MainLayout from '@/components/layout/MainLayout';
-import MainLayout from '@/components/layout/MainLayout';
 
 interface Props {
     children: React.ReactNode,
