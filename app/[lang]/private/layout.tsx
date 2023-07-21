ca117613496699668acfd2ccdecfb6b333e0e620
Lisandro Rocha <lisandro@itcplatform.de>
2023-07-21 12:21:28 +0200

    header now uses User correctly deleted old getUser

--- app/[lang]/private/layout.tsx
+++ app/[lang]/private/layout.tsx
@@ -1,17 +1,21 @@
 // Local imports
 import { Locale } from '@/i18n-config';
 import MainLayout from '@/components/layout/MainLayout';
+import { getUser } from '@/lib/utils/getUser';
 
 interface Props {
     children: React.ReactNode;
     params: { lang: Locale };
 }
 
-const RootLayout = ({
-    children,
-    params: { lang },
-}: Props): React.ReactElement => (
-    <MainLayout lang={lang}>{children}</MainLayout>
-);
+const RootLayout = async ({ children, params: { lang } }: Props) => {
+    const user = await getUser();
+
+    return (
+        <MainLayout lang={lang} user={user}>
+            {children}
+        </MainLayout>
+    );
+};
 
 export default RootLayout;
