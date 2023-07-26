76944a8af5f1bd488bbfd6de8f720341e2f93559
sergioVegaPG <sergio.vega@plattesgroup.net>
2023-07-26 12:33:14 +0200

    Metadata initial commmit

--- components/pages/documents/files/details/DetailsWrapper.tsx
+++ components/pages/documents/files/details/DetailsWrapper.tsx
@@ -76,7 +76,7 @@ export const DetailsWrapper = ({
                     {
                         icon: faDatabase,
                         title: 'Metadata',
-                        children: <FileMetaData />,
+                        children: <FileMetaData document={selectedDocument} />,
                     },
                 ]}
             />
