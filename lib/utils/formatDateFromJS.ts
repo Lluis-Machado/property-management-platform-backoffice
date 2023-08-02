dfaed56d50b3f1506d4c5e14f26077f2cf32fd97
Lisandro Rocha <lisandro@itcplatform.de>
2023-08-02 11:51:57 +0200

    removed console

--- lib/utils/formatDateFromJS.ts
+++ lib/utils/formatDateFromJS.ts
@@ -2,11 +2,9 @@ import { DateTime } from 'luxon';
 
 export const formatDate = (date: Date | string | null) => {
     if (!date) return null;
-    console.log('date es: ', date);
     const luxonDate =
         typeof date === 'string'
             ? DateTime.fromSQL(date as string)
             : DateTime.fromJSDate(date as Date);
-    console.log('luxonDate: ', luxonDate);
     return luxonDate.toFormat('yyyy-MM-dd');
 };
